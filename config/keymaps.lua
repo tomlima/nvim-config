db8f0013f4c78f9fb0f1e1939f8acb23022e90b7
Tom Lima <tom@tomlima.me>
2025-07-23 08:01:28 -0300

    Update keymaps.lua
    Add a new keymap to move lines in normal and visual mode.

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -5,3 +5,13 @@
 --
 vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Abrir terminal" })
 vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n><cmd>ToggleTermToggleAll<CR>]], { noremap = true, silent = true })
+
+-- Move line or visual selection down
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Mover para baixo", silent = true })
+
+-- Move line or visual selection up
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Mover para cima", silent = true })
+
+-- Mover line in normal mode
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "Mover linha para baixo", silent = true })
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { desc = "Mover linha para cima", silent = true })
